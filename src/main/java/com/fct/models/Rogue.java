67846267de68fb59f338b34b8118c5f7678ccb19
Alby Delgado Morales (ext) <alby.delgado.external@eviden.com>
2024-03-21 12:13:47 +0000

    feat: Creation of the models

--- src/main/java/com/fct/models/Rogue.java
+++ src/main/java/com/fct/models/Rogue.java
@@ -1,5 +1,25 @@
 package com.fct.models;
 
-public class Rogue {
+import com.fct.impl.PlayerIMPL;
+
+public class Rogue extends Player implements PlayerIMPL{
+
+	public Rogue(int tag, String username) {
+		super(tag, username);
+	}
+	
+	public Rogue() {
+		
+	}
+
+	public void attack() {
+		System.out.println("Rogue is attacking");
+		
+	}
+
+	public void defense() {
+		System.out.println("Rogue is defending");
+		
+	}
 
 }
